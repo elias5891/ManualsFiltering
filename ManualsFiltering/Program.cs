d88f560b0410e641d9e420335d6ba820accca731
Eliott Adkins <elias5891@gmail.com>
2018-11-07 19:28:26 -0500

    Initial commit from Visual Studio

---
+++ ManualsFiltering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManualsFiltering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
